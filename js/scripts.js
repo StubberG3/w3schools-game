af25991adc47b67fa9d5903fb68d8d0f6eb80572
StubberG3 <StubberG3@gmail.com>
2019-04-06 19:15:27 -0500

    Game Controllers - Keyboard as Controller

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,12 @@ function component(width, height, color, x, y) {
 
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) /* left  */ { myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) /* right */ { myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) /* up    */ { myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) /* down  */ { myGamePiece.speedY = 1; }
     myGamePiece.newPos();
     myGamePiece.update();
 }
@@ -60,6 +66,12 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
