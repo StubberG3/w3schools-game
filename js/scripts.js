c1afb2bddde3770a8deb0bbbe1306b6f8f5dc96d
StubberG3 <StubberG3@gmail.com>
2019-04-06 19:06:46 -0500

    Game Controllers - Get in Control

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,45 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
+  this.speedX = 0;
+  this.speedY = 0;
   this.update = function(){
     ctx = myGameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
+  this.newPos = function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+  }
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.x += 1;
+    myGamePiece.newPos();
     myGamePiece.update();
-  }
+}
+
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+
+function moveright() {
+    myGamePiece.speedX += 1;
+}
+
+function stopMove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+}
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
