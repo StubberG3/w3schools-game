40e07287e378ad44aa29820e111897db4a5b4e15
StubberG3 <StubberG3@gmail.com>
2019-04-06 19:35:01 -0500

    Game Controllers - Controllers on the Canvas

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@ $("document").ready(function() {
 })
 
 function startGame() {
-    myGameArea.start();
     myGamePiece = new component(30, 30, "red", 10, 120);
+    myUpBtn = new component(30, 30, "blue", 50, 10);
+    myDownBtn = new component(30, 30, "blue", 50, 70);
+    myLeftBtn = new component(30, 30, "blue", 20, 40);
+    myRightBtn = new component(30, 30, "blue", 80, 40);
+    myGameArea.start();
 }
 
 function component(width, height, color, x, y) {
@@ -19,18 +23,39 @@ function component(width, height, color, x, y) {
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
-  this.newPos = function() {
-      this.x += this.speedX;
-      this.y += this.speedY;
+  this.clicked = function() {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var clicked = true;
+    if ((mybottom < myGameArea.y) || (mytop > myGameArea.y) || (myright < myGameArea.x) || (myleft > myGameArea.x)) {
+      clicked = false;
+    }
+    return clicked;
   }
 }
 
 function updateGameArea() {
     myGameArea.clear();
     if (myGameArea.x && myGameArea.y) {
-        myGamePiece.x = myGameArea.x;
-        myGamePiece.y = myGameArea.y;
-    }
+        if (myUpBtn.clicked()) {
+          myGamePiece.y -= 1;
+        }
+        if (myDownBtn.clicked()) {
+          myGamePiece.y += 1;
+        }
+        if (myLeftBtn.clicked()) {
+          myGamePiece.x += -1;
+        }
+        if (myRightBtn.clicked()) {
+          myGamePiece.x += 1;
+        }
+      }
+    myUpBtn.update();
+    myDownBtn.update();
+    myLeftBtn.update();
+    myRightBtn.update();
     myGamePiece.update();
 }
 
@@ -60,13 +85,17 @@ var myGameArea = {
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
-        this.canvas.style.cursor = "none"; //hide the original cursor
+        // this.canvas.style.cursor = "none"; //hide the original cursor
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('mousemove', function (e) {
+        window.addEventListener('mousedown', function (e) {
             myGameArea.x = e.pageX;
             myGameArea.y = e.pageY;
+          })
+        window.addEventListener('mouseup', function (e) {
+            myGameArea.x = false;
+            myGameArea.y = false;
         })
     },
     clear : function() {
